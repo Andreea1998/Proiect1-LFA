c3baf1e56cadf89eadf56b867576eec8dc4f48fa
Andreea1998 <ANDREEA.ALDESCU@my.fmi.unibuc.ro>
2018-04-26 10:54:13 +0300

    project2

--- main.cpp
+++ main.cpp
@@ -1,60 +1,83 @@
-#include <iostream>
-#include <fstream>
+#include <cstring>
 using namespace std;
 
-struct vector{
-    char litera, stare1, stare2;
-};
+void citirematrice(char v[100][100][100], int &p) {
+	ifstream f("fisier.in");
+	int x, y;
+	int i, j;
+	int n;
+	char w;
+	f >> p;
+	for (i = 0; i < p; i++)
+		for (j = 0; j < p; j++)
+			v[i][j][0] = '\0';
+	while (f >> x >> y >> w){
+        n=strlen(v[x][y]);
+        v[x][y][n] = w;
+        v[x][y][n+1]='\0';
+	}
+}
 
-bool stare_lamda (char stare, vector v[100], int n){
+void afismatrice(char v[100][100][100], int p) {
+	int i, j;
+	for (i = 0; i < p; i++) {
+		for (j = 0; j < p; j++)
+            if(v[i][j][0]=='\0')
+                cout<<0<<" ";
+			else cout << v[i][j] << " ";
+		cout << endl;
+	}
+}
+
+bool cautare(int starif[50], int j, int m) {
+	int nr;
+	for (nr = 1; nr <= m; nr++)
+		if (j == starif[nr]) return true;
+	return false;
+}
+
+bool cautare_litera(char c[21],char l){
     int i;
-    if (stare=='0')
-        return 1;
-    for (i=0; i<n; i++)
-        if (v[i].stare1==stare && v[i].litera=='0')
-            return 1;
+    int n=strlen(c);
+    for(i=0;i<n;i++)
+        if(c[i]==l) return 1;
     return 0;
 }
 
-void repetitie(vector v[100], char stare, char cuv[21], int &j, int &ok, int n){
-    if(ok==0 && cuv[j]!=NULL && stare!='0'){
-        int i;
-        for(i=0;i<n; i++)
-            if(v[i].stare1==stare && cuv[j]==v[i].litera)
-                j++, repetitie(v, v[i].stare2, cuv, j, ok, n);
-    }
-    else
-    if (cuv[j]==NULL && stare_lamda(stare, v, n)==1)
-        ok=1;
-
-}
-
-void functie( vector v[100], char cuv[21]){
-    ifstream fin("fisier.in");
-    int n,i;
-    char x,y,z;
-    fin>>n;
-    for(i=0;i<n;i++){
-        fin>>x>>y>>z;
-        v[i].litera=x;
-        v[i].stare1=y;
-        v[i].stare2=z;
-    }
-    int j, stare_initiala;
-    j=0;
-    int ok=0;
-    stare_initiala=v[0].stare1;
-    repetitie(v,stare_initiala, cuv, j, ok, n);
-    if (ok==1)
-        cout<<"E cuvant";
-    else cout<<"Nu";
+void AFN(char v[100][100][100], char cuv[21], int starif[50], int poz, int m, int p, int i, int &k) {
+	int j;
+	if (cuv[i] != NULL) {
+		for (j = 0; j < p; j++)
+			if (cautare_litera(v[poz][j],cuv[i])==1) {
+				if (cautare(starif, j, m) && cuv[i + 1] == NULL) {
+					k = 1;
+					return;
+				}
+				else AFN(v, cuv, starif, j, m, p, i + 1, k);
+			}
+		if (k == 0 && j == p) {
+			k = -1;
+			return;
+		}
+	}
 }
 
 int main() {
-    char cuv[21];
-    vector v[100];
-    cout << "Dati cuvantul:" << " ";
-    cin.get(cuv, 21);
-    functie(v,cuv);
-    return 0;
+	char cuv[21], v[100][100][100];
+	int nr, i, c, j, m, p, k, starif[50];
+	citirematrice(v, p);
+	afismatrice(v, p);
+	cout << "Dati cuvantul:" << " ";
+	cin.get(cuv, 21);
+	cout << "Dati numarul de stari finale:" << " ";
+	cin >> m;
+	for (nr = 1; nr <= m; nr++) {
+		cout << "Stare finala" << nr << ":";
+		cin >> starif[nr];
+	}
+	k = 0;
+	AFN(v, cuv, starif, 0, m, p, 0, k);
+	if (k == 1) cout << "Este cuvant";
+	else cout << "Nu este cuvant";
+return 0;
 }
